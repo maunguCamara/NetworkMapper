0040f2688c9959b3374a5472f57052228d596b31
Camara <101089346+maunguCamara@users.noreply.github.com>
2024-08-29 00:24:02 +0300

    Update netmap.h

--- netmap.h
+++ netmap.h
@@ -1,1 +1,16 @@
+#include _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h> 
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+int main(int, char)
