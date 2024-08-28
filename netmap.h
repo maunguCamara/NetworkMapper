1e47df071c5916f4d7a15a96296d7f6c6daeb0e7
Camara <101089346+maunguCamara@users.noreply.github.com>
2024-08-29 00:20:57 +0300

    Update netmap.h

--- netmap.h
+++ netmap.h
@@ -1,9 +1,36 @@
 #include *netmap.h*
-int main (int argc, char *argv[]) {
-    if (argc < 4 ) {
-        fprintf(stderr, 
-        "Usage: %s <port> [start_ip] [stop_ip]\n", *argv[]);
-        return -1;
 
+in_addr_t current, ending;
+in_addr_t generate() {
+    
+    if(current >= ending)
+        return(in_addr_t);
+    else
+        current++;
+    return current;
+}
+
+int main (int argc, char *argv[]) {
+    if (argc < 2 ) {
+        fprintf(stderr, 
+        "Usage: %s <port> [start_ip] [stop_ip]\n", *argv);
+        return -1;
     }
+    if (argc > 2) {
+        current = inet_addr(argv[2]);
+        ending = (argc > 3) ?
+            inet_addr(argv[3]) :
+            inet_addr("255.255.255.255");
+
+        if (
+            (current == INADDR_NONE)
+            || (ending == INADDR_NONE)
+            || (ending < current)
+            ) {
+                    fprintf(stderr, 
+                    "Usage: %s <port> [start_ip] [stop_ip]\n", *argv);
+            return -1;
+            }
+    }
+    return 0;
 }
