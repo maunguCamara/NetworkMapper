981837a6fb80868f01a0175e3097d2860a460bba
Camara <101089346+maunguCamara@users.noreply.github.com>
2024-08-28 23:35:19 +0300

    Update netmap.h

--- netmap.h
+++ netmap.h
@@ -1,1 +1,9 @@
+#include *netmap.h*
+int main (int argc, char *argv[]) {
+    if (argc < 4 ) {
+        fprintf(stderr, 
+        "Usage: %s <port> [start_ip] [stop_ip]\n", *argv[]);
+        return -1;
 
+    }
+}
