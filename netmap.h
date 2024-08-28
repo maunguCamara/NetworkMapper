4807dd6e67643036f5d4db0b847bd1c3b6143ce7
Camara <101089346+maunguCamara@users.noreply.github.com>
2024-08-29 00:32:01 +0300

    Update netmap.h

--- netmap.h
+++ netmap.h
@@ -9,8 +9,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
+in_addr_t generate(void);
 int main(int, char)
